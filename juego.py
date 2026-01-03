17f8d2b413fe9c3cc0be93d767f84838fa69b050
JMiguelFL <juanfaoussi@gmail.com>
2026-01-03 17:59:25 +0100

    Feat: agregar colores y requirements.txt

--- juego.py
+++ juego.py
@@ -1,38 +1,40 @@
 import random
+from colorama import init, Fore, Style 
+
+init(autoreset=True)
 
 def adivina_numero():
-    print("Bienvenido al juego de adivinar el número, estoy pensando en un número del 1 al 100")
-
+    print(Fore.CYAN + "Bienvenido. Estoy pensando en un número del 1 al 100...")
+    
     numero_secreto = random.randint(1, 100)
     intentos = 0
     ganaste = False
-
     historial = []
 
     while not ganaste:
         try:
-            entrada = input("Introduce un número: ")
+            entrada = input(Fore.YELLOW + "Introduce tu número: ")
             numero_usuario = int(entrada)
-
             historial.append(numero_usuario)
-
             intentos += 1
 
-            if numero_usuario > numero_secreto:
-                print("Más bajo")
-            elif numero_usuario < numero_secreto:
-                print("Más alto")
+            if numero_usuario < numero_secreto:
+                print(Fore.BLUE + "Más alto")
+            elif numero_usuario > numero_secreto:
+                print(Fore.BLUE + "Más bajo")
             else:
-                print(f"Felicidades, has adivinado en {intentos} intentos.")
+                print(Fore.GREEN + Style.BRIGHT + f"¡Felicidades! Lo adivinaste en {intentos} intentos.")
                 ganaste = True
+                
         except ValueError:
-            print("Error: Por favor, escribe solo números.")
-        
-    print("\n--- Resumen de tu partida ---")
-    print("Tus intentos fueron: ")
+            print(Fore.RED + "Error: Por favor, escribe solo números.")
 
+    print(Style.BRIGHT + "\n--- Resumen de tu partida ---")
     for numero in historial:
-        print(f"- {numero}")
+        if numero == numero_secreto:
+            print(Fore.GREEN + f"- {numero} (¡Correcto!)")
+        else:
+            print(Fore.RED + f"- {numero}")
 
 if __name__ == "__main__":
     adivina_numero()
